c1f744bc3135a1b3f29a24604889f5d7a3c6de37
Faizan Sorathiya <ayurlandenterprise43@gmail.com>
2026-03-31 10:33:13 +0530

    Title Change

--- Frontend/src/Layout/Layout.jsx
+++ Frontend/src/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "../Layout/Sidebar";
 import Topbar from "./Topbar";
 
 const Layout = () => {
